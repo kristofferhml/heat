d7b9a46857e010b5571f94fdaf2833c92e150533
kristoffer <kristoffer@teletronics.ae>
2024-11-24 00:43:55 +0400

    Save congifured pin

--- heat/heater.py
+++ heat/heater.py
@@ -7,19 +7,19 @@ class Heater():
         GPIO.setup(PIN, GPIO.OUT)
         GPIO.output(PIN, GPIO.HIGH)
         self.current = GPIO.HIGH
-        
+        self.pin = PIN
     def on(self):
         if self.current == GPIO.LOW:
             return
        
-        GPIO.output(PIN, GPIO.LOW)
+        GPIO.output(self.pin, GPIO.LOW)
         self.current = GPIO.LOW
 
     def off(self):
         if self.current == GPIO.HIGH:
             return
         
-        GPIO.output(PIN, GPIO.HIGH)
+        GPIO.output(self.pin, GPIO.HIGH)
         self.current = GPIO.HIGH
        
     def shutdown(self):
